6b898a1b1dbf0a4a7d55544245ef09efb280d14e
hcc <hcc_cqu@163.com>
2021-10-19 16:53:02 +0800

    1

---
+++ gulimall-product/src/test/java/com/hcc/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcc.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
